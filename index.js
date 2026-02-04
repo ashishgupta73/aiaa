375d6ce216a2b683329e70f675bbc64f9d868d8e
gupta <ashish.2428cseai1085@kiet.edu>
2026-02-04 14:31:13 +0530

    BACKEND

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
- node -v
- 
+const fs = require("fs")
+require("dotenv").config
+const express = require("express")
+
+const app = express()
+
+app.get("/data",async(req,res)=>{
+    
+        fs.readFile('data.json', 'utf8',(err,data)=>{
+
+            if(err){
+                return res.status(500).json({
+                    message:`SomeThing IS WRONG ${err}`
+                })
+            }
+
+            const users = JSON.parse(data);
+            const result = users.find(item => item.id == 2);
+            if(!result){
+                return res.status(401).json({
+                    msg:"Id not found"
+                })
+            }
+            res.json(users);
+        });
+    
+
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000")
+})
